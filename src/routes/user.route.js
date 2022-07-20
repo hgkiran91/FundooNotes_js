807a8240d1d13a396d83ce440861317beb79c9b8
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-07-20 15:17:02 +0530

    [kiran]: Refactored login and User Registration

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -11,13 +11,13 @@ router.post('/login', userController.userLogin);
 //route to create a new user
 router.post('', newUserValidator, userController.userRegistration);
 
-//route to get a single user by their user id
+// //route to get a single user by their user id
 // router.get('/:_id', userAuth, userController.getUser);
 
-//route to update a single user by their user id
-router.put('/:_id', userController.updateUser);
+// //route to update a single user by their user id
+// router.put('/:_id', userController.updateUser);
 
-//route to delete a single user by their user id
-router.delete('/:_id', userController.deleteUser);
+// //route to delete a single user by their user id
+// router.delete('/:_id', userController.deleteUser);
 
 export default router;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,6 @@
 import User from '../models/user.model';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 //get all users
 export const userLogin = async (body) => {
@@ -8,38 +10,49 @@ export const userLogin = async (body) => {
   if(data == null){
     throw new Error("User dosen't exist");
   }else{
-    return data;
+    const result = await bcrypt.compare(body.password, data.password);
+    if(result){
+      const token = jwt.sign({ "Id": data._id, "firstName": data.firstName, "email": data.email }, process.env.SECRET_KEY);
+      return token;
+    }else{
+      throw new Error("Invalid Passowrd");
+    }
   }
 };
 
 //create new user
 export const UserRegistration = async (body) => {
+  console.log("Before hassing body:",body);
+  const saltRounds = 10;
+  const hashPassword = await bcrypt.hash(body.password, saltRounds);
+  body.password = hashPassword;
+  console.log("After hassing body:",body);
   const data = await User.create(body);
   return data;
 };
 
-//update single user
-export const updateUser = async (_id, body) => {
-  const data = await User.findByIdAndUpdate(
-    {
-      _id
-    },
-    body,
-    {
-      new: true
-    }
-  );
-  return data;
-};
+// //update single user
+// export const updateUser = async (_id, body) => {
+//   const data = await User.findByIdAndUpdate(
+//     {
+//       _id
+//     },
+//     body,
+//     {
+//       new: true
+//     }
+//   );
+//   return data;
+// };
 
-//delete single user
-export const deleteUser = async (id) => {
-  await User.findByIdAndDelete(id);
-  return '';
-};
+// //delete single user
+// export const deleteUser = async (id) => {
+//   await User.findByIdAndDelete(id);
+//   return '';
+// };
 
-//get single user
-export const getUser = async (id) => {
-  const data = await User.findById(id);
-  return data;
-};
+// //get single user
+// export const getUser = async (id) => {
+//   const data = await User.findById(id);
+//   return data;
+// };
